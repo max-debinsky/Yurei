7a22883f828dbc48db15aeb84ebcb4e03c69beb9
max-debinsky <maksdeb.kontakt@gmail.com>
2024-04-10 23:24:53 +0200

    ping command rework

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -1,11 +1,17 @@
 module.exports = {
     name: 'ping',
-    description: 'Pong!',
+    description: 'Test the pings.',
     // devOnly: Boolean,
     // testOnly: Boolean,
     // options: Object[],
 
-    callback: (client, interaction) => { 
-        interaction.reply(`Pong! ${client.ws.ping}ms`);
+    callback: async (client, interaction) => {
+        await interaction.deferReply();
+
+        const reply = await interaction.fetchReply();
+
+        const ping = reply.createdTimestamp - interaction.createdTimestamp;
+
+        interaction.editReply(`Pong! Client ${ping}ms | Websocket: ${client.ws.ping}ms`);
     }
 }
